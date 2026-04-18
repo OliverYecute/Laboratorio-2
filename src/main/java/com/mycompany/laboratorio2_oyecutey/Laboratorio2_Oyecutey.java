13da4768226a43a5c02b84f5fc4e0258cbabd8fe
OliverYecute <yecuteoliver@gmail.com>
2026-04-18 16:30:00 -0600

    Primer commit

---
+++ src/main/java/com/mycompany/laboratorio2_oyecutey/Laboratorio2_Oyecutey.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.laboratorio2_oyecutey;
+
+/**
+ *
+ * @author yecut
+ */
+public class Laboratorio2_Oyecutey {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
